2c7943997aee438966cf78d846268dc8d472c8c3
Yash <dhikaleyash@gmail.com>
2024-01-02 18:39:45 +0530

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import AuthProvider from "./context/AuthContext";
+import { QueryProvider } from "./lib/react-query/QueryProvide";
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <QueryProvider>
+            <AuthProvider>
+
+                <App />
+            </AuthProvider>
+        </QueryProvider>
+    </BrowserRouter>
+)
